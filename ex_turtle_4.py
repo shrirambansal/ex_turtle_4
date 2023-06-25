3dea9fffc7391fe261fa3a023f193db546e3927c
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-06-25 21:46:56 +0530

    Create ex_turtle_4.py

---
+++ ex_turtle_4.py
@@ -0,0 +1,8 @@
+"""
+Write a program to check whether a number entered by user is even or odd. 
+"""
+num = int(input("Enter the number : "))
+if (num%2==0):
+    print("Even")
+else:
+    print("Odd")
